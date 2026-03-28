3eada6f4108322c1be9568f1e3f881703e5c0914
omer-bashir <mob99pk@gmail.com>
2026-03-28 23:32:39 +0800

    Fix all critical/high/medium reliability issues from audit

---
+++ script.js
@@ -0,0 +1,10 @@
+(function () {
+  var hamburger = document.getElementById('hamburger');
+  var mobileMenu = document.getElementById('mobileMenu');
+  if (hamburger && mobileMenu) {
+    hamburger.addEventListener('click', function () {
+      hamburger.classList.toggle('open');
+      mobileMenu.classList.toggle('open');
+    });
+  }
+})();
